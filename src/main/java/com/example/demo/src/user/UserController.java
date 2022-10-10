bf6b89b4f5411c0337544c86351674a73534949d
yawnsoo <0607yeonsoo@gmail.com>
2022-10-11 06:33:33 +0900

    cicd test complete

--- src/main/java/com/example/demo/src/user/UserController.java
+++ src/main/java/com/example/demo/src/user/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
      */
     //Query String
     @ResponseBody
-    @GetMapping("/test-cicd") // (GET) 127.0.0.1:9000/app/users
+    @GetMapping("") // (GET) 127.0.0.1:9000/app/users
     public BaseResponse<List<GetUserRes>> getUsers(@RequestParam(required = false) String Email) {
         try{
             if(Email == null){
