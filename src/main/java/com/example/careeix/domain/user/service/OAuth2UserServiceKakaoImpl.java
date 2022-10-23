4185e5a7f5959d58ef233801f4006fdab6175ad5
d00hye <dohye11220@gmail.com>
2022-10-23 22:16:18 +0900

    modify

--- src/main/java/com/example/careeix/domain/user/service/OAuth2UserServiceKakaoImpl.java
+++ src/main/java/com/example/careeix/domain/user/service/OAuth2UserServiceKakaoImpl.java
@@ -7,6 +7,7 @@ import com.example.careeix.domain.user.repository.UserRepository;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -44,53 +45,65 @@ public class OAuth2UserServiceKakaoImpl implements OAuth2UserServiceKakao {
 
         HashMap<String, Object> resultMap = new HashMap<>();
 
+        HttpURLConnection conn = null;
         try {
             URL url = new URL(kakaoUserInfoUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
+            conn = (HttpURLConnection) url.openConnection();
+            try {
 
-            //요청에 필요한 Header에 포함될 내용
-            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+                conn.setRequestMethod("GET");
 
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
+                //요청에 필요한 Header에 포함될 내용
+                conn.setRequestProperty("Authorization", "Bearer " + accessToken);
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                int responseCode = conn.getResponseCode();
 
-            String br_line = "";
-            String result = "";
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String br_line = "";
+                    String result = "";
 
-            while ((br_line = br.readLine()) != null) {
-                result += br_line;
+                    while ((br_line = br.readLine()) != null) {
+                        result += br_line;
+                    }
+                    System.out.println("response:" + result);
+
+
+                    JsonParser parser = new JsonParser();
+                    JsonElement element = parser.parse(result);
+
+                    JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+                    JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+                    String id = element.getAsJsonObject().get("id").getAsString();
+                    String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+                    resultMap.put("id", id);
+                    resultMap.put("email", email);
+
+                    System.out.println("결과 : " + resultMap);
+                    br.close();
+                } else if (responseCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
+                    throw new KakaoUnAuthorizedFaildException();
+                } else {
+                    throw new KakaoFailException();
+                }
+            }catch (IOException e) {
+                throw new KakaoApiResponseException();
+            }finally {
+                conn.disconnect();
             }
-            System.out.println("response:" + result);
-
-
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
-            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-
-            String id = element.getAsJsonObject().get("id").getAsString();
-            String email = kakao_account.getAsJsonObject().get("email").getAsString();
-
-            resultMap.put("id", id);
-            resultMap.put("email", email);
-
-            System.out.println("결과 : "+resultMap);
-
-
-
-
+        } catch (MalformedURLException e) {
+            throw new KakaoUrlException(kakaoUserInfoUrl);
+        } catch (ProtocolException e) {
+            throw new KakaoProtocolException();
         } catch (IOException e) {
-            System.err.println(e.toString());
+            throw new KakaoApiResponseException();
         }
         return resultMap;
     }
 
-
-    private User saveOrGetKakaoUser(HashMap<String, Object> kakaoUserInfo) {
+        private User saveOrGetKakaoUser(HashMap<String, Object> kakaoUserInfo) {
         // 회원가입을 한 유저면 반환, 아니면 셋팅
         User user = userRepository.findBySocialId((String) kakaoUserInfo.get("id"))
                 .orElse(User.toEntityOfKakaoUser(kakaoUserInfo));
