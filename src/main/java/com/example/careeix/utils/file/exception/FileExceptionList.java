b0df4c7032214e1cf1dced60a4718a71b1373fa1
d00hye <dohye11220@gmail.com>
2022-10-26 08:39:16 +0900

    modify

--- src/main/java/com/example/careeix/utils/file/exception/FileExceptionList.java
+++ src/main/java/com/example/careeix/utils/file/exception/FileExceptionList.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum FileExceptionList {
-    FILE_EXTENSION("F0001", HttpStatus.BAD_REQUEST, "이미지 파일이 아닙니다."),
-    FILE_LOAD_FAILED("F0002", HttpStatus.BAD_REQUEST, "파일 불러오기에 실패했습니다."),
-    FILE_SAVE_FAILED("F0003", HttpStatus.BAD_REQUEST, "파일 저장에 실패했습니다."),
-    IMAGE_NOT_FOUND("F0004", HttpStatus.NOT_FOUND, "해당 이미지가 존재하지 않습니다.");
+    FILE_EXTENSION("F2001", HttpStatus.BAD_REQUEST, "이미지 파일이 아닙니다."),
+    FILE_LOAD_FAILED("F2002", HttpStatus.BAD_REQUEST, "파일 불러오기에 실패했습니다."),
+    FILE_SAVE_FAILED("F2003", HttpStatus.BAD_REQUEST, "파일 저장에 실패했습니다."),
+    IMAGE_NOT_FOUND("F2004", HttpStatus.NOT_FOUND, "해당 이미지가 존재하지 않습니다.");
 
     private final String CODE;
     private final HttpStatus httpStatus;
--- src/main/java/com/example/careeix/utils/jwt/exception/JwtExceptionList.java
+++ src/main/java/com/example/careeix/utils/jwt/exception/JwtExceptionList.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum JwtExceptionList {
-    NOTFOUNDJWT("J0001", HttpStatus.NOT_FOUND, "JWT 토큰이 비어있습니다."),
-    EXPIRE_ACCESSTOKEN("J0002", HttpStatus.FORBIDDEN, "ACCESS-TOKEN이 만료되었습니다.");
+    NOTFOUNDJWT("J2001", HttpStatus.NOT_FOUND, "JWT 토큰이 비어있습니다."),
+    EXPIRE_ACCESSTOKEN("J2002", HttpStatus.FORBIDDEN, "ACCESS-TOKEN이 틀렸습니다.");
 
     private final String CODE;
     private final HttpStatus httpStatus;
