ed4856a46b691f56d971655e4ee8bdf5c536b9f7
d00hye <dohye11220@gmail.com>
2022-11-07 19:46:09 +0900

    apple

--- src/main/java/com/example/careeix/domain/user/controller/KaKaoController.java
+++ src/main/java/com/example/careeix/domain/user/controller/KaKaoController.java
@@ -2,6 +2,7 @@ package com.example.careeix.domain.user.controller;
 
 import com.example.careeix.config.BaseException;
 import com.example.careeix.domain.user.dto.MessageResponse;
+import com.example.careeix.domain.user.service.AppleServiceImpl;
 import com.example.careeix.domain.user.service.KakaoServiceImpl;
 import com.example.careeix.utils.dto.ApplicationResponse;
 import io.swagger.annotations.Api;
@@ -12,13 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Api(tags ="카카오 API")
+@Api(tags ="소셜 API")
 public class KaKaoController {
 
     private final KakaoServiceImpl kakaoService;
 
-    public KaKaoController(KakaoServiceImpl kakaoService) {
+    private final AppleServiceImpl appleService;
+
+    public KaKaoController(KakaoServiceImpl kakaoService, AppleServiceImpl appleService) {
         this.kakaoService = kakaoService;
+        this.appleService = appleService;
     }
 
     // 회원가입
